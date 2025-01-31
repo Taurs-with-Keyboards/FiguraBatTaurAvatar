79f3de931873a1a85ee10b37b0d23a494988e63e
Total <39064108+TotalTakeover@users.noreply.github.com>
2025-01-31 08:43:39 -0700

    Create BatSounds.lua

---
+++ scripts/BatSounds.lua
@@ -0,0 +1,42 @@
+-- Play sound
+function pings.playBatScreech()
+	
+	if player:isLoaded() then
+		sounds:playSound("entity.bat.ambient", player:getPos(), 0.6, math.random()*0.35+0.85)
+	end
+	
+end
+
+-- Host only instructions
+if not host:isHost() then return end
+
+-- Config setup
+config:name("BatTaur")
+
+-- Variable
+local cooldown = 0
+
+-- Screech Keybind
+local screechBind   = config:load("ScreechKeybind") or "key.keyboard.keypad.2"
+local setScreechKey = keybinds:newKeybind("Bat Screech"):onPress(function() pings.playBatScreech() cooldown = 30 end):key(screechBind)
+
+function events.TICK()
+	
+	-- Reduce cooldown
+	cooldown = math.max(cooldown - 1, 0)
+	
+	-- Disable keybind if cooldown is active, and player isnt dead
+	setScreechKey:enabled(cooldown == 0 and player:getDeathTime() == 0)
+	
+end
+
+-- Keybind updater
+function events.TICK()
+	
+	local screechKey = setScreechKey:getKey()
+	if screechKey ~= screechBind then
+		screechBind = screechKey
+		config:save("ScreechKeybind", screechKey)
+	end
+	
+end
