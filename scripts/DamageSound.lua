7927b2e0030c39a4b6297aef022e7dff997e6242
Total <39064108+TotalTakeover@users.noreply.github.com>
2025-06-07 03:22:49 -0700

    Sound maintenance
    - Removed space lines
    - Removed useless `return`s
    - Added spaces in-between calcs

--- scripts/DamageSound.lua
+++ scripts/DamageSound.lua
@@ -11,12 +11,12 @@ function events.ON_PLAY_SOUND(id, pos, vol, pitch, loop, cat, path)
 	
 	-- If sound contains ".hurt", play an additional hurt sound along side it
 	if id:find(".hurt") then
-		return sounds:playSound("entity.bat.hurt", pos, 0.6, math.random()+0.5)
+		sounds:playSound("entity.bat.hurt", pos, 0.6, math.random() + 0.5)
 	end
 	
 	-- If sound contains ".death", play an additional death sound along side it
 	if id:find(".death") then
-		return sounds:playSound("entity.bat.death", pos, 0.6, math.random()+0.5)
+		sounds:playSound("entity.bat.death", pos, 0.6, math.random() + 0.5)
 	end
 	
 end
